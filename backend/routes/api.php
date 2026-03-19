42130704a7d0eb957508f4f33dc5e64f4f4def20
sajtos.t.gergo <sajtos.t.gergo@dk.akkszalezi.hu>
2026-03-19 12:41:29 +0100

    mar 19, 12:41

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,11 +9,11 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// auth
+// Authentikáció
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// 
-
+// Don't want essay
+Route::get('/dont-want-essay', [UserController::class, 'getDontWantEssay'])->middleware('auth:sanctum');
