f92a9110249bcb23077e46896576d23ea0ed9f19
timudzsin <sajtostimudzsin@gmail.com>
2026-03-23 07:59:33 +0100

    oldalak közötti animált váltás folyamatban

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import UserPage from "./pages/UserPage";
@@ -9,37 +9,111 @@ import UserWantPage from "./pages/UserWantPage";
 import UserGoalsPage from "./pages/UserGoalsPage";
 import UserTasksPage from "./pages/UserTasksPage";
 import { UserProvider } from "./contexts/UserContext";
+import { AnimatePresence, motion } from "framer-motion";
+
+// ez mit csinál?
+function PageWrapper({ children }) {
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 0.03 }}
+		>
+			{children}
+		</motion.div>
+	);
+}
+
+// ez mit csinál?
+function AnimatedRoutes() {
+	const location = useLocation();
+
+	return (
+		<AnimatePresence mode="wait">
+			<Routes location={location} key={location.pathname}>
+				<Route path="/" element={<Navigate to="/register" replace />} />
+
+				<Route
+					path="/register"
+					element={
+						<PageWrapper>
+							<RegisterPage />
+						</PageWrapper>
+					}
+				/>
+				<Route
+					path="/login"
+					element={
+						<PageWrapper>
+							<LoginPage />
+						</PageWrapper>
+					}
+				/>
+
+				<Route
+					path="/user"
+					element={
+						<UserProvider>
+							<PageWrapper>
+								<UserPage />
+							</PageWrapper>
+						</UserProvider>
+					}
+				>
+					<Route index element={<Navigate to="dont-want" replace />} />
+					<Route
+						path="dont-want"
+						element={
+							<PageWrapper>
+								<UserDontWantPage />
+							</PageWrapper>
+						}
+					/>
+					<Route
+						path="want"
+						element={
+							<PageWrapper>
+								<UserWantPage />
+							</PageWrapper>
+						}
+					/>
+					<Route
+						path="goals"
+						element={
+							<PageWrapper>
+								<UserGoalsPage />
+							</PageWrapper>
+						}
+					/>
+					<Route
+						path="tasks"
+						element={
+							<PageWrapper>
+								<UserTasksPage />
+							</PageWrapper>
+						}
+					/>
+				</Route>
+
+				<Route
+					path="/admin"
+					element={
+						<PageWrapper>
+							<AdminPage />
+						</PageWrapper>
+					}
+				/>
+			</Routes>
+		</AnimatePresence>
+	);
+}
 
 function App() {
 	return (
-		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Navigate to="/register" replace />} />
-
-					<Route path="/register" element={<RegisterPage></RegisterPage>}></Route>
-
-					<Route path="/login" element={<LoginPage></LoginPage>}></Route>
-
-					<Route
-						path="/user"
-						element={
-							<UserProvider>
-								<UserPage></UserPage>
-							</UserProvider>
-						}
-					>
-						<Route index element={<Navigate to="dont-want" replace />}></Route>
-						<Route path="dont-want" element={<UserDontWantPage></UserDontWantPage>}></Route>
-						<Route path="want" element={<UserWantPage></UserWantPage>}></Route>
-						<Route path="goals" element={<UserGoalsPage></UserGoalsPage>}></Route>
-						<Route path="tasks" element={<UserTasksPage></UserTasksPage>}></Route>
-					</Route>
-
-					<Route path="/admin" element={<AdminPage></AdminPage>}></Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<AnimatedRoutes />
+		</BrowserRouter>
 	);
 }
 
